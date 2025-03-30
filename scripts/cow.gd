d77e272c25a657aba3893d8ba51da1dc18f27cb3
dopluh <parkerseth14@gmail.com>
2025-03-30 09:17:53 -0600

    can talk to cow

--- scripts/cow.gd
+++ scripts/cow.gd
@@ -1,4 +1,13 @@
-extends Node2D
+extends Area2D
 
-func _process(_delta):
-	pass
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var moo: Label = $InteractionArea/moo
+
+func _on_body_entered(_body: Node2D) -> void:
+	pass # Replace with function body.
+	
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "talk")
+	
+func talk() -> void:
+	moo.show() if moo.visible == false else moo.hide()
